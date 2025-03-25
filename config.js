684dcbac5b8f7bf6b29c6a675c1cdd41c8d4a215
Ahousefavor <110446829+Ahousefavor@users.noreply.github.com>
2025-03-25 09:11:59 -0700

    Create config.js

---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    DB_URI: 'mongodb://localhost:27017/plinko_game',
+    RPC_URL: 'http://localhost:8332',
+    RPC_USER: 'your_rpc_user',
+    RPC_PASSWORD: 'your_rpc_password'
+};
